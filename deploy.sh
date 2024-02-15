22b5b0f8da9a8972f7a68d6974b5d8c7df0fcbb3
Xavier <ladig1017@gmail.com>
2024-02-15 17:08:50 +0900

    deploy seirum coin

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-forge script script/MockSei.s.sol:MockSeiScript --rpc-url https://evm-rpc-arctic-1.sei-apis.com --broadcast
+forge script script/SeirumCoin.s.sol:SeirumCoinDeployScript --rpc-url https://evm-rpc-arctic-1.sei-apis.com --broadcast
